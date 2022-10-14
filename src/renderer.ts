ececfe0ce1c4cbc2f463d2da92e6fd48af8d49b5
youngwooklee210 <wook@zigbang.com>
2022-10-14 12:15:49 +0900

    설정화면 ui 작업 추가

--- src/renderer.ts
+++ src/renderer.ts
@@ -6,9 +6,17 @@
 // needed in the renderer process.
 
 document.getElementById("btn_dev_tool").onclick = function () {
-    alert("btn_dev_tool");
+    alert(`btn_dev_tool`);
 };
 
 document.getElementById("btn_back").onclick = function () {
     alert("btn_back");
+};
+
+document.getElementById("btn_danji").onclick = function () {
+    document.getElementById("main_contents").setAttribute("src", "./options/danjiSetting.html")
+};
+
+document.getElementById("btn_screen_size").onclick = function () {
+    document.getElementById("main_contents").setAttribute("src", "./options/screenSizeSetting.html")
 };
