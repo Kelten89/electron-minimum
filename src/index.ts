61237fbafd702e1adac461ccfa8ac9f5adee2108
youngwooklee210 <wook@zigbang.com>
2022-10-20 17:48:31 +0900

    자동 업데이트 관련 dialog/progressbar 작업 추가

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 // Modules to control application life and create native browser window
-import { app, BrowserWindow, BrowserView, ipcMain } from "electron";
+import { app, BrowserWindow, BrowserView, ipcMain, dialog } from "electron";
 import * as path from "path";
 
 const { autoUpdater } = require("electron-updater")
-const log = require("electron-log")
+const ProgressBar = require("electron-progressbar");
+
+let progressBar = null;
 
 const createWindow = () => {
   // Create the browser window.
@@ -60,25 +62,65 @@ const createWindow = () => {
 };
 
 autoUpdater.on('checking-for-update', () => {
-  log.info('업데이트 확인 중...')
+  console.log('업데이트 확인 중...')
 })
 autoUpdater.on('update-available', (info) => {
-  log.info('업데이트가 가능합니다.')
+  console.log('업데이트가 가능합니다.')
+
+  dialog
+    .showMessageBox({
+      type: "info",
+      title: "Update",
+      message:
+        "새로운 버전이 확인되었습니다. 설치 파일을 다운로드 하시겠습니까?",
+      buttons: ["지금 설치", "나중에 설치"]
+    })
+    .then(result => {
+      const { response } = result;
+
+      if (response === 0) autoUpdater.downloadUpdate();
+    })
 })
 autoUpdater.on('update-not-available', (info) => {
-  log.info('현재 최신버전입니다.')
+  console.log('현재 최신버전입니다.')
 })
 autoUpdater.on('error', (err) => {
-  log.info('에러가 발생하였습니다. 에러내용 : ' + err)
+  console.log('에러가 발생하였습니다. 에러내용 : ' + err)
 })
 autoUpdater.on('download-progress', (progressObj) => {
   let log_message = "다운로드 속도: " + progressObj.bytesPerSecond
   log_message = log_message + ' - 현재 ' + progressObj.percent + '%'
   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
-  log.info(log_message);
+  
+  progressBar = new ProgressBar({
+    text: "Download 합니다."
+  });
+
+  progressBar
+    .on("completed", () => {
+      console.log("설치 완료");
+    })
+    .on("aborted", () => {
+      console.log("aborted");
+    });
 })
 autoUpdater.on('update-downloaded', (info) => {
-  log.info('업데이트가 완료되었습니다.')
+  console.log('업데이트가 완료되었습니다.')
+
+  progressBar.setCompleted();
+
+  dialog
+    .showMessageBox({
+      type: "info",
+      title: "Update",
+      message: "새로운 버전이 다운로드 되었습니다. 다시 시작하시겠습니까?",
+      buttons: ["예", "아니오"]
+    })
+    .then(result => {
+      const { response } = result;
+
+      if (response === 0) autoUpdater.quitAndInstall(false, true);
+    });
 })
 
 // This method will be called when Electron has finished
