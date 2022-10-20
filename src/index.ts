8d921b1f17bf87bf0b54c690d0ff4f1cd520f5d0
youngwooklee210 <wook@zigbang.com>
2022-10-20 14:40:27 +0900

    update 관련 코드 추가

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@
 import { app, BrowserWindow, BrowserView, ipcMain } from "electron";
 import * as path from "path";
 
+const { autoUpdater } = require("electron-updater")
+const log = require("electron-log")
+
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
@@ -56,11 +59,35 @@ const createWindow = () => {
   // view.webContents.openDevTools();
 };
 
+autoUpdater.on('checking-for-update', () => {
+  log.info('업데이트 확인 중...')
+})
+autoUpdater.on('update-available', (info) => {
+  log.info('업데이트가 가능합니다.')
+})
+autoUpdater.on('update-not-available', (info) => {
+  log.info('현재 최신버전입니다.')
+})
+autoUpdater.on('error', (err) => {
+  log.info('에러가 발생하였습니다. 에러내용 : ' + err)
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "다운로드 속도: " + progressObj.bytesPerSecond
+  log_message = log_message + ' - 현재 ' + progressObj.percent + '%'
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
+  log.info(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  log.info('업데이트가 완료되었습니다.')
+})
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
+
+  autoUpdater.checkForUpdates()
 
   app.on("activate", () => {
     // On macOS it's common to re-create a window in the app when the
