a795d4603c47040b6e54d4d4065f40d99a8fad11
youngwooklee210 <wook@zigbang.com>
2022-10-24 14:19:10 +0900

    version 0.0.3

--- src/index.ts
+++ src/index.ts
@@ -52,8 +52,8 @@ const createWindow = () => {
   });
   view.setAutoResize({ width: true, height: true });
   // view.webContents.loadURL("https://electronjs.org");
-  view.webContents.loadURL("http://m.naver.com");
-  // view.webContents.loadURL("http://news.naver.com");
+  // view.webContents.loadURL("http://m.naver.com");
+  view.webContents.loadURL("http://news.naver.com");
   // view.webContents.loadFile('src/index.html');
   // view.webContents.loadURL("https://app.giftistar.net/home");
 
