7e65339b4c43abb23ea534d1317c66bfb43078f1
youngwooklee210 <wook@zigbang.com>
2022-10-26 14:32:38 +0900

    auto update test

--- src/index.ts
+++ src/index.ts
@@ -21,18 +21,6 @@ const createWindow = () => {
     // kiosk: true,
     // skipTaskbar: true,
 
-    // webPreferences: {
-    //   preload: path.join(__dirname, "preload.js"),
-    //   nodeIntegration: false,
-    //   devTools: true,
-    // },
-  });
-
-  // and load the index.html of the app.
-  // mainWindow.loadFile("src/index.html");
-
-  const contentBounds = mainWindow.getContentBounds();
-  const view = new BrowserView({
     webPreferences: {
       devTools: true,
       nodeIntegration:true,
@@ -41,18 +29,32 @@ const createWindow = () => {
       preload: path.join(__dirname, "preload.js"),
     },
   });
-  mainWindow.setBrowserView(view);
-  view.setBounds({
-    x: 0,
-    y: 28,
-    width: contentBounds.width,
-    height: contentBounds.height,
-  });
-  view.setAutoResize({ width: true, height: true });
+
+  // and load the index.html of the app.
+  // mainWindow.loadFile("src/index.html");
+
+  const contentBounds = mainWindow.getContentBounds();
+  // const view = new BrowserView({
+  //   webPreferences: {
+  //     devTools: true,
+  //     nodeIntegration:true,
+  //     contextIsolation: false,
+  //     // preload: path.join(app.getAppPath(), "preload.js"),
+  //     preload: path.join(__dirname, "preload.js"),
+  //   },
+  // });
+  // mainWindow.setBrowserView(view);
+  // view.setBounds({
+  //   x: 0,
+  //   y: 28,
+  //   width: contentBounds.width,
+  //   height: contentBounds.height,
+  // });
+  // view.setAutoResize({ width: true, height: true });
   // view.webContents.loadURL("https://electronjs.org");
-  view.webContents.loadURL("http://m.naver.com");
+  // mainWindow.webContents.loadURL("http://m.naver.com");
   // view.webContents.loadFile('src/index.html');
-  // view.webContents.loadURL("https://app.giftistar.net/home");
+  mainWindow.webContents.loadURL("https://app.giftistar.net/home");
 
   // 다른 창보다 항상 앞선다
   // mainWindow.setAlwaysOnTop(true, "screen-saver");
@@ -69,44 +71,61 @@ autoUpdater.on('update-available', (info) => {
   console.log('업데이트가 가능합니다.')
 
   isUpdateCheckPossible = false
-  // dialog
-  //   .showMessageBox({
-  //     type: "info",
-  //     title: "Update",
-  //     message:
-  //       "새로운 버전이 확인되었습니다. 설치 파일을 다운로드 하시겠습니까?",
-  //     buttons: ["지금 설치", "나중에 설치"]
-  //   })
-  //   .then(result => {
-  //     const { response } = result;
+  dialog
+    .showMessageBox({
+      type: "info",
+      title: "Update",
+      message:
+        "새로운 버전이 확인되었습니다. 설치 파일을 다운로드 하시겠습니까?",
+      buttons: ["지금 설치", "나중에 설치"]
+    })
+    .then(result => {
+      const { response } = result;
 
-  //     if (response === 0) autoUpdater.downloadUpdate();
-  //   })
+      if (response === 0) {
+        autoUpdater.downloadUpdate();
+        if(!progressBar) {
+          progressBar = new ProgressBar({
+            text: "Download 합니다."
+          });
+        
+          progressBar
+            .on("completed", () => {
+              console.log("설치 완료");
+            })
+            .on("aborted", () => {
+              console.log("aborted");
+            });
+        }
+      }
+    })
 })
 autoUpdater.on('update-not-available', (info) => {
   console.log('현재 최신버전입니다.')
 })
-autoUpdater.on('error', (err) => {
+autoUpdater.on('error', (err:string) => {
   console.log('에러가 발생하였습니다. 에러내용 : ' + err)
+  
+  progressBar.setCompleted();
+  progressBar.close();
+
+  dialog
+    .showMessageBox({
+      type: "info",
+      title: "Update",
+      message: `${err}`,
+      buttons: ["예", "아니오"]
+    })
+    .then(result => {
+      // const { response } = result;
+
+      // if (response === 0) autoUpdater.quitAndInstall(false, true);
+    });
 })
 autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "다운로드 속도: " + progressObj.bytesPerSecond
-  log_message = log_message + ' - 현재 ' + progressObj.percent + '%'
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
-  
-  if(!progressBar) {
-    progressBar = new ProgressBar({
-      text: "Download 합니다."
-    });
-  
-    progressBar
-      .on("completed", () => {
-        console.log("설치 완료");
-      })
-      .on("aborted", () => {
-        console.log("aborted");
-      });
-  }
+  // let log_message = "다운로드 속도: " + progressObj.bytesPerSecond
+  // log_message = log_message + ' - 현재 ' + progressObj.percent + '%'
+  // log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
 })
 autoUpdater.on('update-downloaded', (info) => {
   console.log('업데이트가 완료되었습니다.')
@@ -116,20 +135,20 @@ autoUpdater.on('update-downloaded', (info) => {
   progressBar.close();
 
   // electron 종료 & 새 버전 설치 및 재실행
-  autoUpdater.quitAndInstall(false, true);
+  // autoUpdater.quitAndInstall(false, true);
 
-  // dialog
-  //   .showMessageBox({
-  //     type: "info",
-  //     title: "Update",
-  //     message: "새로운 버전이 다운로드 되었습니다. 다시 시작하시겠습니까?",
-  //     buttons: ["예", "아니오"]
-  //   })
-  //   .then(result => {
-  //     const { response } = result;
+  dialog
+    .showMessageBox({
+      type: "info",
+      title: "Update",
+      message: "새로운 버전이 다운로드 되었습니다. 다시 시작하시겠습니까?",
+      buttons: ["예", "아니오"]
+    })
+    .then(result => {
+      const { response } = result;
 
-  //     if (response === 0) autoUpdater.quitAndInstall(false, true);
-  //   });
+      if (response === 0) autoUpdater.quitAndInstall(false, true);
+    });
 })
 
 // This method will be called when Electron has finished
@@ -138,12 +157,12 @@ autoUpdater.on('update-downloaded', (info) => {
 app.whenReady().then(() => {
   createWindow();
 
-  setInterval(()=> {
-    if(isUpdateCheckPossible) {
-      autoUpdater.checkForUpdates()
-      console.log("autoUpdater.checkForUpdates()")
-    }
-  }, 10000)
+  autoUpdater.checkForUpdates()
+  // setInterval(()=> {
+  //   if(isUpdateCheckPossible) {
+  //     console.log("autoUpdater.checkForUpdates()")
+  //   }
+  // }, 10000)
 
   app.on("activate", () => {
     // On macOS it's common to re-create a window in the app when the
